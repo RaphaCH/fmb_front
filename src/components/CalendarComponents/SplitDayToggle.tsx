d3e91abcf8edc98724343520690a9a1bc623dfed
Masanjika <h.masanjika@accenture.com>
2024-03-19 09:39:59 +0100

    Fixed split day toggle

--- src/components/CalendarComponents/SplitDayToggle.tsx
+++ src/components/CalendarComponents/SplitDayToggle.tsx
@@ -33,9 +33,9 @@ const SplitDayToggle = ({
       <input
         type='checkbox'
         value='split-days'
-        defaultChecked={isSplitDay}
+        checked={isSplitDay}
         className='sr-only peer'
-        onClick={onToggle}
+        onChange={onToggle}
       />
       <div className="w-11 h-6 bg-neutral-200 peer-focus:outline-none rounded-full peer dark:bg-neutral-200 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-primary"></div>
       <span className='ml-3 text-sm font-medium text-gray-900 dark:text-gray-300'>
